a8c679fb6b612d4996c28aec7a6156b94b5c9dbf
MaroIsLife <ougnou10@gmail.com>
2021-01-25 11:47:28 +0100

    Update ccmaro.sh

--- ccmaro.sh
+++ ccmaro.sh
@@ -3,15 +3,16 @@
 
 rm -rf ~/.coco*
 rm -rf ~/Library *42*
+rm -rf ~/Library/Caches
 echo '10%
 ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-sleep 0.9
+sleep 0.2
 echo '30%
 ████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-sleep 0.9
+sleep 0.2
 echo '50%
 ██████████████▒▒▒▒▒▒▒▒▒▒'
-sleep 0.9
+sleep 0.2
 echo '100%
 ████████████████████████'
 sleep 0.3
